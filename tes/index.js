133b5293649bd9de371ad920e6a6b18a6c731a6c
Aly Badara Coulibaly <alybadara1803@gmail.com>
2025-07-05 15:30:14 +0100

    update

---
+++ tes/index.js
@@ -0,0 +1,19 @@
+// let yourName = prompt("Enter your name :")
+// let nameEl = document.getElementById("name-el")
+// nameEl.innerText += yourName
+
+let count = 0
+
+let countEl = document.getElementById("count-el")
+
+//increment function
+function increment(){
+    count +=1
+    countEl.innerText = count
+}
+
+//decrement function
+function decrement(){
+    count -= 1;
+    countEl.innerText = count
+}
