c3b718804f3c3fc3377f9c195294d7c4d31ebb0f
Aly Badara Coulibaly <alybadara1803@gmail.com>
2025-07-07 01:10:00 +0100

    Update index.js

--- CalculatorChallenge/index.js
+++ CalculatorChallenge/index.js
@@ -12,7 +12,7 @@ document.getElementById("num2-el").textContent = num2
 // "Sum: 10" (since 8 + 2 = 10) inside the paragraph with id="sum-el"
 let sumEl = document.getElementById("sum-el")
 
-function addition() {
+function add() {
     let result = num1 + num2
     sumEl.textContent = "Sum: " + result
 }
